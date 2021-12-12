bf85ff28664f43a13e70eb8ee30830f940727f13
JumpingPxl <jumpingpxl@gmail.com>
2021-12-12 23:26:11 +0100

    Add dynamic encryption response on upload

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -5,22 +5,12 @@ const Router = express.Router();
 
 Router.post("/", async (req, res) => {
   dataHandler.uploadFile(req).then(file => {
-    res.end("http://localhost:3000/" + file.fileName);
+    res.end((req.secure ? "https" : "http") + "://" + req.headers.host + "/" + file.fileName);
   }).catch(err => {
     console.log("" + err);
     res.writeHead(400);
     res.end(400 + ":" + err);
   });
-
-
-  /*const [error, result] = await dataHandler.uploadFile(req, res);
-  if (error) {
-    console.log(error);
-    res.writeHead(400);
-    res.end(400 + ":" + error);
-  }
-
-  res.end("http://localhost:3000/" + result.fileName); */
 });
 
 module.exports = Router;
